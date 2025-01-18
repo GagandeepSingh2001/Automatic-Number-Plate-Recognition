d5a3922847088cba949688ed6283de7ffe0f6152
GagandeepSingh2001 <gaurav018@outlook.in>
2025-01-18 14:58:47 +0530

    Firebase_Fix

--- frontend/src/firebase.jsx
+++ frontend/src/firebase.jsx
@@ -8,22 +8,15 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBgja72GBCsVSc3ROgZr7WvSbvV9EJuTU0",
-  authDomain: "auth-anpr.firebaseapp.com",
-  projectId: "auth-anpr",
-  storageBucket: "auth-anpr.firebasestorage.app",
-  messagingSenderId: "1082269071416",
-  appId: "1:1082269071416:web:9c979e88169b34824d8ecd",
-  measurementId: "G-MCFW1G40QF"
 
-
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  
 };
 
 // Initialize Firebase
